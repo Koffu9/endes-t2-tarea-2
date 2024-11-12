cbd846f3dfe82a52ce5290dc6b242ce96077d3ec
Raul <koffu9@gmail.com>
2024-11-12 21:32:33 +0100

    Ultimo commit

--- src/endest2/EndesT2Tarea2.java
+++ src/endest2/EndesT2Tarea2.java
@@ -14,7 +14,7 @@ public class EndesT2Tarea2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hola mundo");
+        System.out.println("Hola mundo!");
     }
     
 }
